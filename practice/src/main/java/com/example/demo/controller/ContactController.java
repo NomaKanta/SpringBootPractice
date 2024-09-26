76d4c43059e90a4b61b6cc8976db225310453f9a
NomaKanta <k.noma.cingroup2012@gmail.com>
2024-09-26 17:05:47 +0900

    お問い合わせフォームの実装 その３

--- practice/src/main/java/com/example/demo/controller/ContactController.java
+++ practice/src/main/java/com/example/demo/controller/ContactController.java
@@ -1,36 +1,92 @@
-
-//このクラスのパッケージを表しています。
 package com.example.demo.controller;
 
-//ここから必要なパッケージをインストールしています。
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.data.ContactData;
-//ここまで必要なパッケージをインストールしています。
+import com.example.demo.entity.Contact;
+import com.example.demo.form.ContactForm;
+import com.example.demo.repository.ContactRepository;
 
-@Controller //システムに「このクラスはコントローラだよ！認識しておいてね！」と宣言します。
+@Controller
 public class ContactController {
-    @PostMapping("/contact") //HTTPのPOSTリクエストを処理するメソッドを表します。
-    //@ModelAttributeアノテーションを使って、Dataクラスに定義したオブジェクトをContactControllerクラスのconfirmメソッドの引数に設定します。
-    //具体的に引数は@ModelAttribute ContactData contactDataとしてあげることで、
-    //confirmメソッドの中でcontactData.getLastName()のようにContactDataクラス(Lombokの@Dataアノテーション)の機能を使うことができます。
-    public ModelAndView contact(@ModelAttribute ContactData contactData, ModelAndView mv) {
 
-        mv.setViewName("confirmation");
+	@Autowired
+	private ContactRepository contactRepository;
 
-        mv.addObject("lastName", contactData.getLastName());
-        mv.addObject("firstName", contactData.getFirstName());
-        mv.addObject("email", contactData.getEmail());
-        mv.addObject("phone", contactData.getPhone());
-        mv.addObject("zipCode", contactData.getZipCode());
-        mv.addObject("address", contactData.getAddress());
-        mv.addObject("buildingName", contactData.getBuildingName());
-        mv.addObject("contactType", contactData.getContactType());
-        mv.addObject("body", contactData.getBody());
+	@GetMapping("/contact")
+	public String contact(Model model) {
+		model.addAttribute("contactForm", new ContactForm());
 
-        return mv;
-    }
+		return "contact";
+	}
+
+	@PostMapping("/contact")
+	public String contact(@Validated @ModelAttribute("contactForm") ContactForm contactForm, BindingResult errorResult,
+			HttpServletRequest request) {
+
+		if (errorResult.hasErrors()) {
+			return "contact";
+		}
+
+		HttpSession session = request.getSession();
+		session.setAttribute("contactForm", contactForm);
+
+		return "redirect:/contact/confirm";
+	}
+
+	@GetMapping("/contact/confirm")
+	public String confirm(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+
+		ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
+		model.addAttribute("contactForm", contactForm);
+		return "confirmation";
+	}
+
+	@PostMapping("/contact/register")
+	public String register(Model model, HttpServletRequest request) {
+
+		HttpSession session = request.getSession();
+		ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
+
+		Contact contact = new Contact();
+		contact.setLastName(contactForm.getLastName());
+		contact.setFirstName(contactForm.getFirstName());
+		contact.setEmail(contactForm.getEmail());
+		contact.setPhone(contactForm.getPhone());
+		contact.setZipCode(contactForm.getZipCode());
+		contact.setAddress(contactForm.getAddress());
+		contact.setBuildingName(contactForm.getBuildingName());
+		contact.setContactType(contactForm.getContactType());
+		contact.setBody(contactForm.getBody());
+
+		contactRepository.save(contact);
+
+		return "redirect:/contact/complete";
+	}
+
+	@GetMapping("/contact/complete")
+	public String complete(Model model, HttpServletRequest request) {
+
+		if (request.getSession(false) == null) {
+			return "redirect:/contact";
+		}
+
+		HttpSession session = request.getSession();
+		ContactForm contactForm = (ContactForm) session.getAttribute("contactForm");
+		model.addAttribute("contactForm", contactForm);
+
+		session.invalidate();
+
+		return "completion";
+	}
 }
